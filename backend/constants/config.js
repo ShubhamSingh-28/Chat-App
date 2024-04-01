0c61a83be5673b4a367eb547a40bbaf338c55b5a
Shubham <ss9971768704@gmail.com>
2024-04-01 15:47:47 +0530

    h

--- backend/constants/config.js
+++ backend/constants/config.js
@@ -1,8 +1,8 @@
 const corsOptions = {
     origin: [
+      process.env.CLIENT_URL,
       "http://localhost:5173",
       "http://localhost:4173",
-      process.env.CLIENT_URL,
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
